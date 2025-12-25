dd73cd7d959434b22ad755b477ce328ec1aeb67a
meyal111 <brenda.02.salas@gmail.com>
2025-12-25 15:31:51 -0500

    Agrega modelos y migraciones del inventario

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'sku',
+        'barcode',
+        'price',
+        'category_id',
+    ];
+
+    //Relación uno a muchos inversa
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //Relación uno a muchos
+    public function inventories()
+    {
+        return $this->hasMany(Inventory::class);
+    }
+
+    //Relación polimórfica
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+
+}
